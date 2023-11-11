04c3e65b88efd5c051c3f85b9b178bf1fb0e32bd
leonardo.lang <leonardo.lang@hftm.ch>
2023-11-11 11:43:49 +0100

    feat: Add basic layout for quiz question

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,91 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        // Header container
+        appBar: AppBar(
+          leading: const Icon(Icons.quiz),
+          title: const Text('Header'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                debugPrint('Menu button is pressed');
+              },
+            ),
+          ],
+        ),
+        body: const SafeArea(
+          child: Card(
+            elevation: 12.5,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.question_mark),
+                  title: Center(
+                    child: Text('Question 1'),
+                  ),
+                  subtitle: Center(
+                    child: Text('History'),
+                  ),
+                  trailing: Icon(Icons.close),
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Some question?",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    AnswerTile(answerText: "Answer 1"),
+                    SizedBox(height: 10),
+                    AnswerTile(answerText: "Answer 2"),
+                    SizedBox(height: 10),
+                    AnswerTile(answerText: "Answer 3"),
+                    SizedBox(height: 10),
+                    AnswerTile(answerText: "Answer 4"),
+                    SizedBox(height: 10),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class AnswerTile extends StatelessWidget {
+  const AnswerTile({
+    super.key,
+    required this.answerText,
+  });
+
+  final String answerText;
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: width * 0.95,
+      padding: const EdgeInsets.all(10),
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: Colors.black,
+          side: const BorderSide(color: Colors.black),
+        ),
+        onPressed: () => debugPrint("Pressed $answerText"),
+        child: Text(answerText),
+      )
     );
   }
 }
