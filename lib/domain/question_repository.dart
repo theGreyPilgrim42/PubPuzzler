6360246ba762584044818eeaccc35ada1ce53fe5
leonardo.lang <leonardo.lang@hftm.ch>
2023-12-20 21:40:13 +0100

    feat: Add QuestionRepository

---
+++ lib/domain/question_repository.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:pub_puzzler/domain/question.dart';
+
+class QuestionRepository {
+  int _nextId = 1;
+  List<Question> _questions = [];
+  // Singleton approach
+  static final QuestionRepository _questionRepository = QuestionRepository._privateConstructor();
+  QuestionRepository._privateConstructor();
+
+  factory QuestionRepository() {
+    return _questionRepository;
+  }
+
+  Future<void> addQuestion(Question question) async {
+    question.id = _nextId++;
+    _questions.add(question);
+  }
+
+  Future<List<Question>> getQuestions() async {
+    return _questions;
+  }
+
+  Future<Question> getQuestion(int id) async {
+    return _questions.firstWhere((question) => question.id == id);
+  }
+
+  Future<void> removeQuestion(int id) async {
+    _questions.removeWhere((question) => question.id == id);
+  }
+}
