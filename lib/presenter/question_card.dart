416488a442f59512eeefcf8927279fb88e0087cf
leonardo.lang <leonardo.lang@hftm.ch>
2024-02-20 17:06:55 +0100

    refactor(presenter): Use Providers

--- lib/presenter/question_card.dart
+++ lib/presenter/question_card.dart
@@ -54,9 +54,10 @@ class _QuestionCardState extends State<QuestionCard>
       question.then((q) {
         if (answer == q.correctAnswer) {
           q.answerState = AnswerState.correct;
-          widget.gameProvider.updateScore();
+          widget.gameProvider.updateCurrentGame(true, true);
         } else {
           q.answerState = AnswerState.incorrect;
+          widget.gameProvider.updateCurrentGame(false, false);
         }
         logger.d('Answer is: ${q.correctAnswer}');
       });
@@ -76,126 +77,135 @@ class _QuestionCardState extends State<QuestionCard>
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 12.5,
-        child: FutureBuilder<Question>(
-          future: question,
-          builder: (BuildContext context, AsyncSnapshot<Question> snapshot) {
-            List<Widget> children;
-            if (snapshot.hasData) {
-              children = <Widget>[
-                Card(
-                  elevation: 12.5,
-                  margin: const EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      ListTile(
-                        leading: const Icon(Icons.question_mark),
-                        title: Center(
-                          child: Text('Question #$questionNumber'),
-                        ),
-                        subtitle: Center(
-                          child: Text(snapshot.data!.category.name.toString()),
-                        ),
-                        trailing: IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: const Icon(Icons.close),
-                        ),
+      elevation: 12.5,
+      child: FutureBuilder<Question>(
+        future: question,
+        builder: (BuildContext context, AsyncSnapshot<Question> snapshot) {
+          List<Widget> children;
+          if (questionNumber > widget.gameProvider.maxRounds) {
+            children = <Widget>[
+              Card(
+              elevation: 12.5,
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    const Text("Game Finished"),
+                    ElevatedButton(
+                      onPressed: () {
+                        widget.gameProvider.persistCurrentGame();
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Go Back"),
+                    )
+                  ]
+                )
+            ),
+            ];
+          }
+          else if (snapshot.hasData) {
+            children = <Widget>[
+              Card(
+                elevation: 12.5,
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    ListTile(
+                      leading: const Icon(Icons.question_mark),
+                      title: Center(
+                        child: Text('Question #$questionNumber'),
                       ),
-                      Column(
-                        children: [
-                          LinearProgressIndicator(
-                            value: animationController.value,
+                      subtitle: Center(
+                        child: Text(snapshot.data!.category.name.toString()),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        }, 
+                        icon: const Icon(Icons.close),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        LinearProgressIndicator(
+                          value: animationController.value,
+                        ),
+                        Text(
+                          snapshot.data!.question,
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
+                        ),
+                        for (var answer in snapshot.data!.answers) 
+                          AnswerTile(
+                            answerText: answer, 
+                            checkAnswer: checkAnswer, 
+                            answered: answered, 
+                            correctAnswer: snapshot.data!.correctAnswer,
+                          ),
+                        if (answered) 
                           Text(
-                            snapshot.data!.question,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            snapshot.data!.answerState == AnswerState.correct ? 'Correct!' : 'Incorrect!',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: snapshot.data!.answerState == AnswerState.correct ? Theme.of(context).primaryColor : Theme.of(context).colorScheme.error,
+                            ),
                           ),
-                          for (var answer in snapshot.data!.answers)
-                            AnswerTile(
-                              answerText: answer,
-                              checkAnswer: checkAnswer,
-                              answered: answered,
-                              correctAnswer: snapshot.data!.correctAnswer,
-                            ),
-                          if (answered)
-                            Text(
-                              snapshot.data!.answerState == AnswerState.correct
-                                  ? 'Correct!'
-                                  : 'Incorrect!',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: snapshot.data!.answerState ==
-                                        AnswerState.correct
-                                    ? Theme.of(context).primaryColor
-                                    : Theme.of(context).colorScheme.error,
-                              ),
-                            ),
-                        ],
-                      ),
-                    ],
-                  ),
+                      ],
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: !answered
-                      ? null
-                      : () {
-                          setState(() {
-                            question = fetchQuestion(
-                                category: widget.category.id,
-                                difficulty: widget.difficulty.id);
-                            answered = false;
-                            questionNumber++;
-                            countdown();
-                          });
-                        },
-                  child: const Text('Next question'),
-                ),
-              ];
-            } else if (snapshot.hasError) {
-              logger.e(snapshot.error.toString());
-              children = <Widget>[
-                Icon(
-                  Icons.error_outline,
-                  color: Theme.of(context).colorScheme.error,
-                  size: 60,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: Text('Error: ${snapshot.error}'),
-                ),
-              ];
-            } else {
-              children = const <Widget>[
-                SizedBox(
-                  width: 60,
-                  height: 60,
-                  child: CircularProgressIndicator(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16),
-                  child: Text('Awaiting result...'),
-                ),
-              ];
-            }
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: children,
               ),
-            );
-          },
-        ));
+              ElevatedButton(
+                onPressed: !answered ? null : () {
+                  setState(() {
+                    question = fetchQuestion(category: widget.category.id, difficulty: widget.difficulty.id);
+                    answered = false;
+                    questionNumber++;
+                    countdown();
+                  });
+                }, 
+                child: const Text('Next question'),
+              ),
+            ];
+          } else if (snapshot.hasError) {
+            logger.e(snapshot.error.toString());
+            children = <Widget>[
+              Icon(
+                Icons.error_outline,
+                color: Theme.of(context).colorScheme.error,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            ];
+          } else {
+            children = const <Widget>[
+              SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text('Awaiting result...'),
+              ),
+            ];
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: children,
+            ),
+          );
+        },      
+      )
+    );
   }
 }
--- lib/presenter/question_screen.dart
+++ lib/presenter/question_screen.dart
@@ -22,23 +22,21 @@ class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
     return Consumer<GameProvider>(
-        builder: (context, game, child) => Scaffold(
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Current Score: ${game.score}',
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                  QuestionCard(
-                      gameProvider: game,
-                      category: widget.category,
-                      difficulty: widget.difficulty),
-                ],
+      builder: (context, game, child) => Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Current Score: ${game.currentGame.score}',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ));
+            ),
+            QuestionCard(gameProvider: game, category: widget.category, difficulty: widget.difficulty),
+          ],
+        ),
+      )
+    );
   }
 }
