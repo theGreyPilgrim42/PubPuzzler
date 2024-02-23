c459b68cb8a4314a1b513e3edf057dba8e6f6443
leonardo.lang <leonardo.lang@hftm.ch>
2024-02-23 14:13:06 +0100

    feat(datasources): Add method to execute Logging cloud function

--- lib/external/datasources/functions.dart
+++ lib/external/datasources/functions.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:global_configuration/global_configuration.dart';
+import 'package:pub_puzzler/domain/entities/question.dart';
 import 'package:pub_puzzler/infra/services/logger_util.dart';
 
 final _logger = getLogger();
@@ -11,19 +12,28 @@ final _client = Client()
     .setProject(dotenv.env['APPWRITE_PROJECT_ID']);
 final _functions = Functions(_client);
 
-Future<Map<String, dynamic>> getExecutionResult(String functionId) async {
+Future<Map<String, dynamic>> _getPostExecutionResult(String functionId, Map<String, String> body, Map<dynamic, dynamic> headers) async {
   try {
     final execution = await _functions.createExecution(
         functionId: functionId,
-        body: json.encode({'test': 'test'}),
+        body: json.encode(body),
         xasync: false,
         path: '/',
-        method: 'GET',
-        headers: {});
+        method: 'POST',
+        headers: headers);
     return execution.toMap();
-  } on AppwriteException catch (e) {
-    _logger.e(
-        "An error occurred while getting execution results - ${e.message} - ${e.type} - ${e.code.toString()}");
+  } on AppwriteException catch(e) {
     throw Future.error("An error occurred while getting execution results");
   }
 }
+
+Future<void> executeLogErrorFunction(String errorMsg) async {
+  try {
+    final result = await _getPostExecutionResult(dotenv.env['APPWRITE_FUNCTION_ERROR_LOGGING_ID']!, {'errorMsg': errorMsg}, {});
+    final statusCode = result['responseStatusCode'];
+
+    if (statusCode != 201) _logger.e('An error occurred while requesting to execute logging function - StatusCode: $statusCode');
+  } catch(e) {
+    logger.e(e);
+  }
+  }
